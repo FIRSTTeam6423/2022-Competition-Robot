eade8b783438c7c0fe58a37ad6a29caf081a4483
gsola05 <gabeonlinemedia@yahoo.com>
2022-02-01 20:47:25 -0500

    Added comments to CargoState
    Added comments to CargoState describing what each state does

--- src/main/java/frc/robot/enums/CargoState.java
+++ src/main/java/frc/robot/enums/CargoState.java
@@ -2,5 +2,10 @@ package frc.robot.enums;
 
 public enum CargoState {
     IDLE,INTAKE,INDEX,SPINUP,SHOOT;
-    
+    /**Idle - No motors are running
+     * Intake - Ball magnet and Low Indexer are running
+     * Index - Low Indexer is running
+     * Spinup - High indexer in running and Shooter is speeding up
+     * Shoot - Shooter is at spead and High Indexer is running
+     */
 }
