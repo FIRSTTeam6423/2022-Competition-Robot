690b6afde5203e4072882c6a78892dda29b8219a
gsola05 <gabeonlinemedia@yahoo.com>
2022-02-17 15:49:32 -0500

    Added three commands for the competition while in auto
    Created three new commands for auto, one for just getting out of the tarmac, one for shooting and then getting out of the tarmac, and one for going out of the tarmac picking up a ball and going back to shoot it

--- src/main/java/frc/robot/commands/autoCommands/DriveForTime.java
+++ src/main/java/frc/robot/commands/autoCommands/DriveForTime.java
@@ -30,7 +30,7 @@ public class DriveForTime extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        driveUtil.tankDrive(.5, -.5);
+        driveUtil.tankDrive(.25, -.25);
         System.out.println("Drive for time works");
     }
 
